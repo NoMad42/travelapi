a7dab57e63cc5cfa7e4b92bcb3a84578ee8d1a43
NoMad42 <nomad42@nomad42.ru>
2023-06-15 01:20:50 +0300

    add filters and sort to tour route

--- app/Http/Controllers/Api/V1/TourController.php
+++ app/Http/Controllers/Api/V1/TourController.php
@@ -3,17 +3,55 @@
 namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\TourListRequest;
 use App\Http\Resources\Api\V1\TourRecource;
-use App\Models\Tour;
 use App\Models\Travel;
-use Illuminate\Http\Request;
 
 class TourController extends Controller
 {
-    public function index(Travel $travel)
+    public function index(Travel $travel, TourListRequest $request)
     {
         return TourRecource::collection(
             $travel->tours()
+                ->when(
+                    $request->priceFrom,
+                    fn($query) => $query->where(
+                        'price',
+                        '>=',
+                        $request->priceFrom * 100
+                    )
+                )
+                ->when(
+                    $request->priceTo,
+                    fn($query) => $query->where(
+                        'price',
+                        '<=',
+                        $request->priceTo * 100
+                    )
+                )
+                ->when(
+                    $request->dateFrom,
+                    fn($query) => $query->where(
+                        'starting_date',
+                        '>=',
+                        $request->dateFrom
+                    )
+                )
+                ->when(
+                    $request->dateTo,
+                    fn($query) => $query->where(
+                        'starting_date',
+                        '<=',
+                        $request->dateTo
+                    )
+                )
+                ->when(
+                    $request->sortBy && $request->sortOrder,
+                    fn($query) => $query->orderBy(
+                        $request->sortBy,
+                        $request->sortOrder
+                    )
+                )
                 ->orderBy('starting_date')
                 ->paginate()
         );
